7df048a8a0c9301c4b34f2dcfb9997ee3737d8ad
DESKTOP-FMFOUAL\baage <yjchoi9212@gmail.com>
2022-01-18 11:20:46 -0600

    update functions based on json
    1)updated cssdata.csv_to_dataframe to be based on json and confirmed the result. 2)removed all the magic numbers in cssdata.csv_to_datafram

--- cssdata.py
+++ cssdata.py
@@ -1,53 +1,39 @@
 # import from existing libraries
 import pandas as pd
 from matplotlib import pyplot as plt
+import json
 
-# import from my modules
-from import_data import data_dirs
-
-
-def csv_to_dataframe(basedir, experiments, drs):
+def csv_to_dataframe(input, exp_id, trial_id, num_headers=5):
     """
-    make a list to contain dataframes for the whole data
-    :param basedir: file directory where the raw data is saved
-    :param experiments: a list of integer that specifies the experiment numbers that you are targeting
-    :param drs: relative density of each trial
-    :return: a list of dataframes that contains each `.csv` file
+    convert csv to dataframe based on the specified exp_id and trial_id
+    note that `input.json` data must be provided to input argument
+    :param input: `input.json` data
+    :param exp_id: experiment id
+    :param trial_id: trial id
+    :param num_headers: number of headers in csv file
+    :return: dataframe
     """
-    dfs = []  # make an empty list to contain dataframes
-
-    # get the number of trials for each experiment
+    experiments = input['experiments']
     for experiment in experiments:
-        datadirs = data_dirs(experiment, basedir=basedir)  # get dir list of trial files in each exp
-        num_experiments = len(experiments)  # get num of experiments
-        num_trials = len(datadirs)  # get num of trials in each exp
+        # iterate over experiments and find the experiment that corresponds to `exp_id` specified.
+        if experiment['exp_id'] == exp_id:
+            trials = experiment['trials']
+        for trial in trials:
+            # iterate over trials and fine the trial that corresponds to `trial_id` specified.
+            if trial['id'] == trial_id:
+                file = trial['file']
+                relative_density = trial['relative_density']
+                # convert csv to dataframe
+                df = pd.read_csv(f"./rawdata/Experiment-{exp_id}/{file}", header=num_headers)
+                # add relative density to the first column of the dataframe.
+                df.insert(loc=0, column="Dr [%]", value=relative_density)
+                # compute and insert ru at the 5th column of the dataframes
+                # confining pressure value of the test is located at the first row of effective vertical stress column.
+                confining_pressure = df['Effective Vertical Stress [kPa]'][0]
+                ru = df['Excess Pore Pressure [kPa]']/confining_pressure
+                df.insert(loc=5, column="ru", value=ru)
 
-        # make a list to contain a dataframes for each trial in the exp
-        df_trial = []
-
-        # get dataframes for a single trial and append it to `df_trial`
-        for trial in range(num_trials):
-
-            # make a dataframes for each trial
-            df_single = pd.read_csv(datadirs[trial], header=5)  # make `.csv` to pandas `df`
-
-            # insert Dr values at the first column of the dataframes
-            df_single.insert(0, "Dr [%]", drs[experiment - 7][trial])
-
-            # compute and insert ru at the 4th column of the dataframes
-            # confining pressure of the test is the first (starting) effective vertical stress
-            confining_pressure = df_single.iloc[0, 4]  # get conf pressure of the trial
-            ru = df_single['Excess Pore Pressure [kPa]']/confining_pressure
-            df_single.insert(5, "ru", ru)
-
-            # Append this dataframes for the trial to `df_trial`
-            df_trial.append(df_single)
-
-        # Append `df_trial` to `dfs`
-        dfs.append(df_trial)
-
-    return dfs
-
+    return df
 
 def plot_trial(dataframes, expindex, trialindex):
     """plot all the columns in the dataframes at expindex and trialindex"""
@@ -62,27 +48,4 @@ def plot_trial(dataframes, expindex, trialindex):
         axi.set(xlabel='Data point')
         axi.set(ylabel=data_col_names[i])
     plt.show()
-
-
-def print_experiment_summary(dataframes, timeindex, exp_num_list):
-    """
-    Some of data has a irregular time interval and different data points.
-    This function look into those.
-    """
-    for k in range(len(exp_num_list)):
-        len_data_list = len(dataframes[k])  # return num of trials at `k`th experiment
-        print(f"Experiment{exp_num_list[k]}----------------------------------------")
-
-        for i in range(len_data_list):
-            print(f"*Trial-{i+1}")
-            df = dataframes[k][i]  # load dataframes for the specified exp-trial
-            index = df.index  # get the index of the dataframes
-            index_last = index[-1]  # get the last index
-            time = df['Time [sec]']  # get the time steps (sec)
-            time_last = time[index_last]  # get the last time step
-            time_choose = time[timeindex]  # get the time (sec) at the specified index
-            time_interval = time_last/index_last  # get the time interval (sec) between each data point
-
-            print(f"last index and time:{index_last}, {time_last}; interval: {time_interval:.4f}\n{timeindex} time step is: {time_choose} sec")
-
 
--- train.py
+++ train.py
@@ -1,27 +1,13 @@
 # %%
 import os
 import json
-
 import cssdata
 
 # %% import data and dataframe
 
-cwd = os.getcwd()  # get current working directory
-data_dir = os.path.join(cwd, "rawdata")  # define the path for the rawdata
-inputfile = "input.json"  # name of the `input.json`
-inputfile_dir = os.path.join(cwd, inputfile)  # define the path for the `input.json`
-
 # open the `input.json` file
-with open(inputfile_dir, "r") as f:
-    json_data = json.load(f)
-
-experiments = json_data["exp_num_list"]  # experiment list that you are importing
-dr_exp7 = json_data["dr_exp7"]  # relative density of each trial in experiment 7
-dr_exp8 = json_data["dr_exp8"]  # relative density of each trial in experiment 8
-dr_exp9 = json_data["dr_exp9"]  # relative density of each trial in experiment 9
-dr_exp10 = json_data["dr_exp10"]  # relative density of each trial in experiment 10
-drs = [dr_exp7, dr_exp8, dr_exp9, dr_exp10]  # collect relative densities in a list
+input = json.load(open("input.json", "r"))
 
 # get dataframes for all trials
-dfs = cssdata.csv_to_dataframe(basedir=data_dir, experiments=experiments, drs=drs)
+cssdata.csv_to_dataframe(input=input, exp_id=7, trial_id=1)
 
