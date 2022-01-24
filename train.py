982817a29c115b501d8524adeea8882eb4da5c1a
DESKTOP-FMFOUAL\baage <yjchoi9212@gmail.com>
2022-01-24 17:15:10 -0600

    drop ./ in jason

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import cssdata
 input = json.load(open("input.json", "r"))
 
 # get dataframes for a trial
-cssdata.csv_to_dataframe(input=input, exp_id=9, trial_id=4)
+cssdata.csv_to_dataframe(input=input, exp_id=7, trial_id=3)
 
 # # plot a trial
 # cssdata.plot_trial(input=input, exp_id=7, trial_id=1)
