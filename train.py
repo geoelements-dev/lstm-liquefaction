201f592b14b6c923ebfbb8bccdaaa68dd142c85c
DESKTOP-FMFOUAL\baage <yjchoi9212@gmail.com>
2022-01-31 16:35:18 -0600

    update preparedata.py

--- train.py
+++ train.py
@@ -1,15 +1,32 @@
 # %%
-import os
 import json
-import cssdata
+import preparedata
+
 
 # %% import data and dataframe
 
 # open the `input.json` file
+
 input = json.load(open("input.json", "r"))
 
-# get dataframes for a trial
-cssdata.csv_to_dataframe(input=input, exp_id=7, trial_id=3)
+# %% select exp-trials to use for train & test
 
-# # plot a trial
-# cssdata.plot_trial(input=input, exp_id=7, trial_id=1)
+train_ids = input["train_ids"]
+test_ids = input["test_ids"]
+
+train_dfs = preparedata.select_datasets(dataset_ids=train_ids)
+test_dfs = preparedata.select_datasets(dataset_ids=test_ids)
+
+# %% normalize the shear stress with the confining pressure and the time step with the final time for each selected dataset
+
+normalized_train_dfs = preparedata.normalize(dfs=train_dfs)
+normalized_test_dfs = preparedata.normalize(dfs=test_dfs)
+
+# %% set features and target, sample the datasets based on time window sampling
+
+rnn_data_train = preparedata.rnn_inputs(
+    dfs=normalized_train_dfs, features=input["features"], targets=input["targets"], window_length=input["window_length"]
+)
+rnn_data_test = preparedata.rnn_inputs(
+    dfs=normalized_test_dfs, features=input["features"], targets=input["targets"], window_length=input["window_length"]
+)
